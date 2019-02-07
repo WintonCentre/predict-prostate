aef52734a18a164f03195ec3d142103c7920f71c
Jin Park <JinPark.Han@gmail.com>
2019-02-07 11:59:49 +0000

    publish script uses logged in username

--- publish-staging.sh
+++ publish-staging.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av resources/public/ jin@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
+rsync -av resources/public/ $USER@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
