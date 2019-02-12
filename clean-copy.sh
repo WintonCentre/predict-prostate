5a80b73e21edfb2da8b7dabed3b8cea610ed0f4c
Jin Park <JinPark.Han@gmail.com>
2019-02-12 11:31:06 +0000

    Script to make clean copy

--- clean-copy.sh
+++ clean-copy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#cp .gitignore .temp_exclude_list
 cat .gitignore exclude_list.txt > .temp_exclude_list
-rsync -av --exclude-from=.temp_exclude_list /Users/jin/Workspace/predict-prostate  /Users/jin/Workspace/predict-prostate-copy-03
+rsync -av --exclude-from=.temp_exclude_list /Users/jin/Workspace/predict-prostate  /Users/jin/Workspace/predict-prostate-copy-06
