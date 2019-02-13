a1ac3f5c207a23050189ccd5da066823c78a2636
Jin Park <JinPark.Han@gmail.com>
2019-02-13 11:17:52 +0000

    Debugging

--- resources/public/sw_cache_update.js
+++ resources/public/sw_cache_update.js
@@ -26,7 +26,7 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
     return caches.open(CACHE).then(function (cache) {
         return cache.addAll([
-            // '/',
+            '/',
             '/index.html',
             '/manifest.json',
             '/assets/favicon.png',
--- resources/public/sw_net_cache.js
+++ resources/public/sw_net_cache.js
@@ -24,6 +24,7 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
     return caches.open(CACHE).then(function (cache) {
         return cache.addAll([
+            '/',
             '/index.html',
             '/manifest.json',
             '/assets/favicon.png',
