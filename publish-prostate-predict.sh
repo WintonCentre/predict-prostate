11a8d5741b88fd757739a1ac1081b1d20fe574d2
gmp26 <gmp26@cam.ac.uk>
2019-02-13 07:56:19 +0000

    many lighthouse prompted tweaks

--- publish-prostate-predict.sh
+++ publish-prostate-predict.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av resources/public/ $USER@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
+rsync -av --del --no-perms --no-owner --no-group resources/public/ $USER@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
