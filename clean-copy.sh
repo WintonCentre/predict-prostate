da2b47d2d621700e15c898c05c78af89c28c1e0f
Jin Park <JinPark.Han@gmail.com>
2019-02-15 15:18:07 +0000

    Change clean script to use user's directory

--- clean-copy.sh
+++ clean-copy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cat .gitignore exclude_list.txt > .temp_exclude_list
-rsync -av --exclude-from=.temp_exclude_list /Users/jin/Workspace/predict-prostate  /Users/jin/Workspace/predict-prostate-copy-06
+#rsync -av --exclude-from=.temp_exclude_list /Users/$USER/Workspace/predict-prostate  /Users/$USER/Workspace/predict-prostate-clean
+rsync -av --exclude-from=.temp_exclude_list $(pwd) $(pwd)-clean
