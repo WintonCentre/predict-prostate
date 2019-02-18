ad8d08d6faab49b6c9935f50a7abb672a0a079c9
gmp26 <gmp26@cam.ac.uk>
2019-02-18 22:34:34 +0000

    removed debug, trying to add disabled +/- when limits reached

--- src/predict_prostate/components/numeric_input.cljs
+++ src/predict_prostate/components/numeric_input.cljs
@@ -35,8 +35,7 @@
 (def timer (atom nil))
 
 (defn validate [value nmin nmax step]
-  (let [_ (js/console.log "incdec nmin " nmin " nmax " nmax " value " value " step " step)
-        value (str-to-num value)
+  (let [value (str-to-num value)
         nmin (if (fn? nmin) (nmin) nmin)
         nmax (if (fn? nmax) (nmax) nmax)                    ;(if (keyword? nmax) @(input-cursor nmax) nmax)
         val-1 (if (js/isNaN value)                          ; is value blank?
@@ -49,11 +48,9 @@
                 value)
         val-2 (+ step val-1)                                ; do the increment
 
-        ; todo adjust behaviour to allow single digit entry
-
-
+        ; todo: can we disable inc/dec buttons? if necessary?
         val-3 (if (< val-2 nmin)                            ; is it too small?
-                (str nmin ":" val-2)                        ; yes
+                (str (if (pos? val-2) val-2 0) ":" val-2)   ; yes
                 (if (> val-2 nmax)                          ; no; is it too big?
                   (str nmax ":" val-2)                      ; yes, return good and bad values, in colon separated string
                   val-2))]                                  ; no
@@ -61,50 +58,49 @@
 
 (defn handle-inc [value onChange nmin nmax step]
   (let [v (validate value nmin nmax step)]
-    (js/console.log "onChange " v)
+    ;(js/console.log "onChange " v)
     (onChange v)))
 
 
 (defn handle-typed-input [nmin nmax onChange e]
 
-  (let [el (-> e .-target)
-        value (.. el -value)
-        ok (re-matches #"\s*\d+\s*" value)
-        ]
-    (if ok
-      (let [value (str-to-num value)
-            val-3 (validate value nmin nmax 0)]
-        (js/console.log "handle-typed-input: value = " val-3)
-        (js/console.log "handle-typed-input: max = " nmax)
-        (onChange (str val-3)))
+  (let [value (.. (-> e .-target) -value)]
+    (if (re-matches #"\s*\d+\s*" value)
+      (onChange (str (validate (str-to-num value) nmin nmax 0)))
       (onChange (num-to-str ##NaN)))
     ))
 
 (defn update-value [value nmin nmax step onChange e]
   (let [value (str-to-num value)]
 
-    (js/console.log "update value = " value)
-    (js/console.log "update value nmin = " nmin)
-    (js/console.log "update value nmax = " nmax)
+    ;(js/console.log "update value = " value)
+    ;(js/console.log "update value nmin = " nmin)
+    ;(js/console.log "update value nmax = " nmax)
     (handle-inc value onChange nmin nmax step)
     ))
 
-(rum/defcs inc-dec-button < rum/static (rum/local nil ::timer)
+(rum/defcs inc-dec-button < rum/static rum/reactive (rum/local nil ::timer)
   [state
-   {:keys [cursor increment onChange min max]
+   {:keys [cursor increment onChange min max nmin nmax]
     :as   props}]
-  (let [start-timer (fn [e] (js/setInterval #(handle-inc @cursor onChange min max increment) 250))]
+  (let [value (rum/react cursor)
+        start-timer (fn [e] (js/setInterval #(handle-inc @cursor onChange nmin nmax increment) 250))]
     [:span {:class-name "incdec"}
-     [:a {:class-name    (str (if (pos? increment) "right" "left") " btn btn-default")
-          :aria-hidden   "true"
-          :tab-index     -1
-          :on-mouse-down #(do (reset! (::timer state) (start-timer %)))
-          :on-mouse-up   #(do (js/clearInterval @(::timer state))
-                              (reset! (::timer state) nil))
-          :on-mouse-out  #(do (js/clearInterval @(::timer state))
-                              (reset! (::timer state) nil))
-          :on-click      #(update-value @cursor min max increment onChange %)
-          }
+     ;
+     ;todo: for some reason I was using a butonified :a  here. Keep an eye out for issues with :button
+     ;
+     [:button {:class-name    (str (if (pos? increment) "right" "left") " btn btn-default ")
+               :aria-hidden   "true"
+               :disabled      (if (pos? increment)
+                                (if (>= value (str-to-num (if (fn? max) (max) max))) "disabled" nil)
+                                (if (<= value nmin) "disabled" nil))
+               :tab-index     -1
+               :on-mouse-down #(do (reset! (::timer state) (start-timer %)))
+               :on-mouse-up   #(do (js/clearInterval @(::timer state))
+                                   (reset! (::timer state) nil))
+               :on-mouse-out  #(do (js/clearInterval @(::timer state))
+                                   (reset! (::timer state) nil))
+               :on-click      #(update-value @cursor nmin nmax increment onChange %)}
       (if (pos? increment) "+" "–")]]))
 
 
@@ -112,33 +108,31 @@
 (rum/defc numeric-input < rum/static rum/reactive           ;echo-update
   [{:keys [input-ref onChange min max error-color color] :or {error-color "red" color "black"} :as props}]
 
-  (js/console.log "props: " props)
+  ;(js/console.log "props: " props)
   (let [[good bad] (split (rum/react input-ref) #":")
         #_#_value (str-to-num (rum/react input-ref))
         value (str-to-num good)
-        nmin (str-to-num (if (fn? min) (min) min))          ;(if (keyword? min) (rum/react (input-cursor min)) min))
-        nmax (str-to-num (if (fn? max) (max) max) #_(if (keyword? max) (rum/react (input-cursor max)) max))
+        nmin (str-to-num (if (fn? min) (min) min))
+        nmax (str-to-num (if (fn? max) (max) max))
         mutate (fn [e]
-                 (js/console.log "mutating, :nmin = " nmin " value = " value " bad = " bad)
+                 ;(js/console.log "mutating, :nmin = " nmin " value = " value " bad = " bad)
                  (handle-typed-input (str-to-num (if (fn? min) (min) min))
-                                     (str-to-num (if (fn? max) (max) max))
-                                     onChange e))]
+                   (str-to-num (if (fn? max) (max) max))
+                   onChange e))]
 
     [:div {:id          "numeric-input"
            :style       {:width      "130px"
                          :tab-index  1
                          :selectable true}
            :on-key-down #(let [key-code (.. % -nativeEvent -code)]
-                           #_(.preventDefault %)
+                           (.preventDefault %)
                            (handle-inc value onChange nmin nmax
-                                       (cond
-                                         #_#_(= "ArrowRight" key-code) 5
-                                         (= "ArrowUp" key-code) 1
-                                         (= "ArrowDown" key-code) -1
-                                         #_#_(= "ArrowLeft" key-code) -5
-                                         :else 0)))}
+                             (cond
+                               (= "ArrowUp" key-code) 1
+                               (= "ArrowDown" key-code) -1
+                               :else 0)))}
      [:button-group.form-control
-      (inc-dec-button (assoc props :increment -1 :cursor input-ref))
+      (inc-dec-button (assoc props :nmin nmin :nmax nmax :increment -1 :cursor input-ref))
       [:input
        {:type      "text"
         :value     good                                     ;(num-to-str value)
@@ -153,7 +147,7 @@
                     :text-align       "center"
                     #_#_:font-weight "bold"}
         }]
-      (inc-dec-button (assoc props :increment 1 :cursor input-ref))
+      (inc-dec-button (assoc props :nmin nmin :nmax nmax :increment 1 :cursor input-ref))
       ]]
 
     ))
