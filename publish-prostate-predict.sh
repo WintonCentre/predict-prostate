1b9e0d98566bad6ce8597739cc7c81dcbb2251c5
gmp26 <gmp26@cam.ac.uk>
2019-02-22 14:45:26 +0000

    added set -e and build to publish script

--- publish-prostate-predict.sh
+++ publish-prostate-predict.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+set -e
+lein clean
+lein cljsbuild once min
 rsync -av --del --no-perms --no-owner --no-group resources/public/ $USER@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
