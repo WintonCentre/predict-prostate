858b72fbb2855aa3e312054c3faade1cdda5a061
Jin Park <JinPark.Han@gmail.com>
2019-03-06 09:41:14 +0000

    Update clean copy script

--- clean-copy.sh
+++ clean-copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat .gitignore exclude_list.txt > .temp_exclude_list
 #rsync -av --exclude-from=.temp_exclude_list /Users/$USER/Workspace/predict-prostate  /Users/$USER/Workspace/predict-prostate-clean
-rsync -av --exclude-from=.temp_exclude_list $(pwd) $(pwd)-clean
+rsync -av --exclude-from=.temp_exclude_list $(pwd) $(pwd)--clean
