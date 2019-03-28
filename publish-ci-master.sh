2aca73deaf416a77e44115640f84ea7ea2f8fd00
Jin Park <jinpark.han@gmail.com>
2019-03-28 14:56:29 +0000

    Master dry run

--- publish-ci-master.sh
+++ publish-ci-master.sh
@@ -8,7 +8,7 @@ LOCAL_DEV='resources/public/'
 #lein clean
 #lein cljsbuild once min
 echo "===============rsync-ing files==================="
-rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET
+rsync --dry-run -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET
 echo "================================="
 echo "Uploaded to " $TARGET
 echo "================================="
