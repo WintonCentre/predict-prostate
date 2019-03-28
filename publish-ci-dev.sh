0f4f9814d7bee1d5b308fe278094afde331a78f1
Jin Park <jinpark.han@gmail.com>
2019-03-28 15:48:43 +0000

    Remove build in ci script

--- publish-ci-dev.sh
+++ publish-ci-dev.sh
@@ -4,9 +4,6 @@ set -e
 echo "Publishing to Development server"
 TARGET='/var/www/prostate.predict.wintoncentre.uk/'
 LOCAL_DEV='resources/public/'
-#echo "===============Building files============="
-#lein clean
-#lein cljsbuild once min
 echo "===============rsync-ing files==================="
 rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $TARGET
 echo "================================="
