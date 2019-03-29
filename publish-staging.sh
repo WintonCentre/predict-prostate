c2e8b0e84f4ac77f0d5c70f974bf0a9ddf2f2b08
Jin Park <jinpark.han@gmail.com>
2019-03-29 10:45:29 +0000

    Improve manual publish script to have compression

--- publish-staging.sh
+++ publish-staging.sh
@@ -5,10 +5,10 @@ echo "Publishing to Staging server"
 TARGET='prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/'
 LOCAL_DEV='resources/public/'
 echo "===============Building files============="
-lein clean
-lein cljsbuild once min
+#lein clean
+#lein cljsbuild once min
 echo "===============rsync-ing files==================="
-rsync -av --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET
+rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET
 echo "================================="
 echo "Uploaded to " $TARGET
 echo "================================="
