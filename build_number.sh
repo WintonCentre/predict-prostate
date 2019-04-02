7e9df8961037df1e5d99a5415a82e14a72b0b79a
Jin Park <jinpark.han@gmail.com>
2019-04-02 12:34:07 +0100

    Add build number

---
+++ build_number.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+echo "===============Adding build number============="
+BUILD_SEARCH="Build: v0.0-dev-#000-hash"
+GIT_DESCRIBE=$(git describe)
+
+echo ${BUILD_SEARCH}
+echo ${GIT_DESCRIBE}
+
+sed -i "s|${BUILD_SEARCH}|${GIT_DESCRIBE}|g" resources/public/js/compiled/predict_prostate.js
