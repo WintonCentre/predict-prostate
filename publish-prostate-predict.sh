b4a10435fe61aa72c2657a0f7ba42432a0a28ee7
gmp26 <gmp26@cam.ac.uk>
2019-04-02 11:16:22 +0100

    deleted test.sh

--- publish-prostate-predict.sh
+++ publish-prostate-predict.sh
@@ -2,4 +2,4 @@
 set -e
 lein clean
 lein cljsbuild once min
-rsync -av --del --no-perms --no-owner --no-group resources/public/ $USER@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
+rsync -av --del --no-perms --git pull no-owner --no-group resources/public/ $USER@prostate.predict.wintoncentre.uk:/var/www/prostate.predict.wintoncentre.uk/
