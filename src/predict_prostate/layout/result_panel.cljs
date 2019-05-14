7342154e582e5f75229a02bf54112e3f3cf2b247
Jin Park <jinpark.han@gmail.com>
2019-05-14 11:15:47 +0100

    Disable logging

--- src/predict_prostate/layout/result_panel.cljs
+++ src/predict_prostate/layout/result_panel.cljs
@@ -39,7 +39,8 @@
      (if content
        (do (try (.ga js/window "send" "event" "Results Tab" label)
                 (catch js/Object e
-                  (.log js/console e))
+                  #_(.log js/console e)
+                  )
                 ) (content))
        [:p "No content yet"])
      ;(.log js/console "content called: " label)
