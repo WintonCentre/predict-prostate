dea6a1288f519c160015eb5d703dda56f849b02b
gmp26 <gmp26@cam.ac.uk>
2020-04-03 12:37:23 +0100

    build-number.sh updated for PP-11

--- build_number.sh
+++ build_number.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "===============Adding build number============="
-BUILD_SEARCH="Build: v0.0-dev-#000-hash"
+BUILD_SEARCH="v-0.0-0.00-0-hash"
 GIT_DESCRIBE=$(git describe)
 
 echo ${BUILD_SEARCH}
