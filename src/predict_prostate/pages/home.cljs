4ecd9a8d9d12da39ddad1b83d49a3274f870f49d
Mehdi <mehdihajjam@gmail.com>
2024-09-11 09:35:57 +0200

    PP-81 - editing out remaining references to predict.nhs.uk domain

--- src/predict_prostate/pages/home.cljs
+++ src/predict_prostate/pages/home.cljs
@@ -56,7 +56,7 @@
        (start-button-group ttt supported-languages)
 
        [:p {:style {:margin-left 15}}
-        [:i (ttt [:home/dymtv "Did you mean to visit"]) " "] [:a {:href "https://breast.predict.nhs.uk"} "Predict Breast Cancer"] (ttt [:home/qmark "?"])]
+        [:i (ttt [:home/dymtv "Did you mean to visit"]) " "] [:a {:href "https://breast.predict.cam"} "Predict Breast Cancer"] (ttt [:home/qmark "?"])]
        [:p {:style {:margin-left 15
                     :color "#4c4c6b"}}
         (ttt [:home/notice "As Predict's usage grows, we have moved to a new URL. Please adjust your bookmarks."])]]
@@ -134,13 +134,13 @@
           [:.col-sm-2 {:key 7 :style {:font-size 16 :margin-left 0}} (ttt [:zh-yue "Cantonese"])]
           [:.col-sm-9 {:key 8 :style {:font-size 16 :margin-left 0}}
            [:a {:href   "https://youtu.be/jCnXBCd13c8"
-                :target "_blank"} "PREDICT預報前列線網上工具——幫助初確診前列線癌病患作出醫療決定：https://prostate.predict.nhs.uk"]]
+                :target "_blank"} "PREDICT預報前列線網上工具——幫助初確診前列線癌病患作出醫療決定：https://prostate.predict.cam"]]
 
 
           [:.col-sm-2 {:key 9 :style {:font-size 16 :margin-left 0}} (ttt [:zh-cmn "Mandarin"])]
           [:.col-sm-9 {:key 10 :style {:font-size 16 :margin-left 0}}
            [:a {:href   "https://youtu.be/7sGtrnNOWE0"
-                :target "_blank"} "PREDICT 预报前列线网上工具--帮助初确诊前列线癌病患作出医疗决定: https://prostate.predict.nhs.uk"]]
+                :target "_blank"} "PREDICT 预报前列线网上工具--帮助初确诊前列线癌病患作出医疗决定: https://prostate.predict.cam"]]
 
           [:.col-sm-2 {:key 11 :style {:font-size 16 :margin-left 0}} (ttt [:es "Spanish"])]
           [:.col-sm-9 {:key 12 :style {:font-size 16 :margin-left 0}}
